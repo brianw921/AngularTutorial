a8360f16c2b7fc5e4d1626121ef5adc5c2872337
Brian Wong <bwong92@gmail.com>
2020-12-28 21:34:15 -0500

    CreatingComponents

---
+++ src/app/courses.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+//Declarator function. 
+@Component({ 
+    selector: 'courses', //<courses>
+    template: '<h2>Courses</h2>'
+
+}) 
+export class CoursesComponent{
+
+}
