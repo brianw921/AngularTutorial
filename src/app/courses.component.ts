ca705c7cc2648c61f36de675fc6ed78fde7fd4ac
Brian Wong <bwong92@gmail.com>
2021-01-04 15:04:05 -0500

    Services

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,10 +3,23 @@ import {Component} from '@angular/core';
 //Declarator function. 
 @Component({ 
     selector: 'courses', //<courses>
-    template: '<h2>Courses</h2>'
+    template: `
+                <h2>{{ title }}</h2>
+                <ul>
+                    <li *ngFor="let course of courses">
+                        {{ course }}
+                    </li>
+                </ul>
+                `
 
 }) 
 
 export class CoursesComponent{
+    title = "List of courses";
+
+    // courses = ["course1" , "course2", "course3"];
+
+    //HTTP endpoint should be delegated to services. 
+    courses;
 
 }
